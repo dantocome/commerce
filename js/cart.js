ed4147fd9f6b2990c672f8a8f14e67ec12253ec2
Daniel Egele <danielegele9@gmail.com>
2024-01-11 22:52:52 +0100

    all added files

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,18 @@
 const carts = document.querySelectorAll(".add-cart");
 
 for(let i =0; i < carts.length; i++){
-    console.log("run");
+    carts[i].addEventListener("click", () => {
+        cartNumbers();
+    })
+}
+function cartNumbers(){
+    localStorage.setItem("cartNumbers", 1);
+    let productNumbers = localStorage.getItem("cartNumbers");
+    productNumbers = parseInt(productNumbers);
+
+    console.log(productNumbers);
+    
+
+    
+
 }
